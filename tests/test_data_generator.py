2acb769e60744a2ae8fab9de544d150a31ac295c
Adam Kells <adamjkells93@gmail.com>
2024-04-22 16:00:12 +0100

    Added data generator

--- tests/test_data_generator.py
+++ tests/test_data_generator.py
@@ -8,6 +8,9 @@ def test_doppeldata_integrate():
 
 
 
+# The data generator and the data model are tightly coupled.
+# We need to write a test that will fail if one is changed and the other is not.
+
 
 # Run the tests
 if __name__ == "__main__":
