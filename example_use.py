165db95dc0b6135f62f988308397d446de768259
jenniferajiang <jennigato95@gmail.com>
2024-05-19 16:28:01 +0100

    Update example use

--- example_use.py
+++ example_use.py
@@ -1,46 +1,71 @@
 from healthchain.use_cases.cds import ClinicalDecisionSupport
 from healthchain.decorators import ehr, api, sandbox
-
 import dataclasses
-import uuid
+from pydantic import BaseModel
+import random
+import json
 
 
 def run():
+    class MockBundle(BaseModel):
+        resource: str = "medication"
+
     @dataclasses.dataclass
     class synth_data:
         context: dict
-        uuid: str
-        prefetch: dict
+        resources: MockBundle
+
+    class DataGenerator:
+        def __init__(self) -> None:
+            self.workflow = None
+
+        def set_workflow(self, workflow):
+            self.workflow = workflow
+
+        def generate(self, constraint):
+            examples = ["medication", "problems", "procedures"]
+            data = MockBundle(resource=random.choice(examples))
+            data = synth_data(context={}, resources=data)
+            print(
+                f"This is synthetic FHIR data from the generator, the param is {constraint}, the workflow is {self.workflow}"
+            )
+
+            return data
 
     @sandbox(service_config={"port": 9000})
     class myCDS(ClinicalDecisionSupport):
         def __init__(self) -> None:
-            self.data_generator = None
+            self.data_generator = DataGenerator()
+            self.chain = self.define_chain()
+
+        def define_chain(self):
+            return "This will be processed by llm"
+            # llm = OpenAI()
+            # prompt = ""
+            # parser = JsonOutputParser()
+            # chain = prompt | llm | parser
+            # return chain
 
         # decorator sets up an instance of ehr configured with use case CDS
         @ehr(workflow="encounter-discharge", num=3)
-        def load_data(self, data_spec):
-            # data = "hello, " + data_spec
-            data = synth_data(
-                context={
-                    "userId": "Practitioner/123",
-                    "patientId": data_spec,
-                    "encounterId": "123",
-                },
-                uuid=str(uuid.uuid4()),
-                prefetch={},
-            )
+        def load_data(self):
+            data = self.data_generator.generate(constraint=["long_duration"])
             return data
 
         @api
         def llm(self, text: str):
-            print(f"processing {text}...")
+            # result = self.chain.invoke(text)
+            # return result
+            print(text)
+            request = json.loads(text)
+            prefetch = request.get("prefetch")
+            resource = prefetch.get("resource", "")
             return {
                 "cards": [
                     {
-                        "summary": "example",
+                        "summary": self.chain,
                         "indicator": "info",
-                        "source": {"label": "website"},
+                        "source": {"label": resource},
                     }
                 ]
             }
