299b8b00c6374746ee42fad426e3ce42caf84bdb
jenniferjiangkells <jenniferjiangkells@gmail.com>
2024-06-21 18:30:31 +0100

    Added model dump and class methods to cda models

--- healthchain/models/requests/cdarequest.py
+++ healthchain/models/requests/cdarequest.py
@@ -1,5 +1,33 @@
+import base64
+import xmltodict
+
 from pydantic import BaseModel
+from typing import Dict
+
+from healthchain.utils.cdaparser import search_key_from_xml_string
 
 
 class CdaRequest(BaseModel):
     document: str
+
+    @classmethod
+    def from_dict(cls, data: Dict):
+        """
+        Loads data from dict (xmltodict format)
+        """
+        return cls(document=xmltodict.unparse(data))
+
+    def model_dump(self, *args, **kwargs) -> Dict:
+        """
+        Dumps document as dict with xmltodict
+        """
+        return xmltodict.parse(self.document)
+
+    def model_dump_xml(self, *args, **kwargs) -> str:
+        """
+        Decodes and dumps document as an xml string
+        """
+        document = search_key_from_xml_string(self.document, "urn:Document")
+        cda = base64.b64decode(document).decode("UTF-8")
+
+        return cda
--- healthchain/models/responses/cdaresponse.py
+++ healthchain/models/responses/cdaresponse.py
@@ -1,7 +1,34 @@
+import base64
+import xmltodict
+
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Dict
+
+from healthchain.utils.cdaparser import search_key_from_xml_string
 
 
 class CdaResponse(BaseModel):
     document: str
-    error: Optional[str]
+    error: Optional[str] = None
+
+    @classmethod
+    def from_dict(cls, data: Dict):
+        """
+        Loads data from dict (xmltodict format)
+        """
+        return cls(document=xmltodict.unparse(data))
+
+    def model_dump(self, *args, **kwargs) -> Dict:
+        """
+        Dumps document as dict with xmltodict
+        """
+        return xmltodict.parse(self.document)
+
+    def model_dump_xml(self, *args, **kwargs) -> str:
+        """
+        Decodes and dumps document as an xml string
+        """
+        document = search_key_from_xml_string(self.document, "tns:Document")
+        cda = base64.b64decode(document).decode("UTF-8")
+
+        return cda
