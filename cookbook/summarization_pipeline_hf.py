a2951669978279765cbe88a180fb718d31f9764a
jenniferjiangkells <jenniferjiangkells@gmail.com>
2024-11-14 15:26:14 +0000

    Added cookbook

---
+++ cookbook/summarization_pipeline_hf.py
@@ -0,0 +1,68 @@
+import healthchain as hc
+
+from healthchain.pipeline import SummarizationPipeline
+from healthchain.use_cases import ClinicalDecisionSupport
+from healthchain.models import CdsFhirData, CDSRequest, CDSResponse
+from healthchain.data_generators import CdsDataGenerator
+
+from langchain_huggingface.llms import HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import getpass
+import os
+
+
+if not os.getenv("HUGGINGFACEHUB_API_TOKEN"):
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = getpass.getpass("Enter your token: ")
+
+
+@hc.sandbox
+class DischargeNoteSummarizer(ClinicalDecisionSupport):
+    def __init__(self):
+        # Initialize pipeline and data generator
+        chain = self._init_chain()
+        self.pipeline = SummarizationPipeline.load(
+            chain, source="langchain", template_path="templates/cds_card_template.json"
+        )
+        self.data_generator = CdsDataGenerator()
+
+    def _init_chain(self):
+        hf = HuggingFaceEndpoint(
+            repo_id="HuggingFaceH4/zephyr-7b-beta",
+            task="text-generation",
+            max_new_tokens=512,
+            do_sample=False,
+            repetition_penalty=1.03,
+        )
+        model = ChatHuggingFace(llm=hf)
+        template = """
+        You are a bed planner for a hospital. Provide a concise, objective summary of the input text in short bullet points separated by new lines,
+        focusing on key actions such as appointments and medication dispense instructions, without using second or third person pronouns.\n'''{text}'''
+        """
+        prompt = PromptTemplate.from_template(template)
+        chain = prompt | model | StrOutputParser()
+
+        return chain
+
+    @hc.ehr(workflow="encounter-discharge")
+    def load_data_in_client(self) -> CdsFhirData:
+        # Generate synthetic FHIR data for testing
+        data = self.data_generator.generate(
+            free_text_path="data/discharge_notes.csv", column_name="text"
+        )
+        return data
+
+    @hc.api
+    def my_service(self, request: CDSRequest) -> CDSResponse:
+        # Process the request through our pipeline
+        result = self.pipeline(request)
+        return result
+
+
+if __name__ == "__main__":
+    # Start the sandbox server
+    summarizer = DischargeNoteSummarizer()
+    summarizer.start_sandbox()
