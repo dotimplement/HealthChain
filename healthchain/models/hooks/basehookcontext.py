caf1e8352cb71a35407ed31708b46d6ab75c891e
jenniferjiangkells <jenniferjiangkells@gmail.com>
2025-02-19 19:50:01 +0000

    Add encounterId to context model

--- healthchain/models/hooks/basehookcontext.py
+++ healthchain/models/hooks/basehookcontext.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 from abc import ABC
 
 
 class BaseHookContext(BaseModel, ABC):
     userId: str
     patientId: str
+    encounterId: Optional[str] = None
