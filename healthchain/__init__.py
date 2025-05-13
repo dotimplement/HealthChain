b2beda7793e87e04ae71fef77c2311c746f7a0c4
jenniferjiangkells <jenniferjiangkells@gmail.com>
2025-05-13 10:08:11 +0100

    Fix namespace conflict

--- healthchain/__init__.py
+++ healthchain/__init__.py
@@ -5,7 +5,7 @@ from .utils.logger import add_handlers
 from .config.base import ConfigManager, ValidationLevel
 
 # Sandbox imports for backwards compatibility
-from .sandbox import sandbox, api, ehr
+from .sandbox.decorator import sandbox as sandbox_decorator, api, ehr
 
 # Enable deprecation warnings
 warnings.filterwarnings("always", category=DeprecationWarning, module="healthchain")
@@ -16,4 +16,7 @@ add_handlers(logger)
 logger.setLevel(logging.INFO)
 
 # Export them at the top level
-__all__ = ["ConfigManager", "ValidationLevel", "sandbox", "api", "ehr"]
+__all__ = ["ConfigManager", "ValidationLevel", "sandbox_decorator", "api", "ehr"]
+
+# For backwards compatibility
+sandbox = sandbox_decorator
